eb45ad85dd390c423eb90c0ea990a88d3a43186d
Thai Anh Duc <anhduc.thai@gmail.com>
2021-10-14 16:16:05 +0700

    Add application project. They are all NET6.0. Cool

---
+++ code/Whatever/Whatever.EventLike.Api/Controllers/CountersController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Whatever.EventLike.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CountersController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/increase")]
+        public async Task<IActionResult> Increase()
+        {
+            await Task.CompletedTask;
+            return Ok();
+        }
+    }
+}
